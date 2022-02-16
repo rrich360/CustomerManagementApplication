38a8fd7ad79e62f708dfd6f0025043f2063a9afd
Richards <roger.richards@accenturefederal.com>
2022-02-16 05:22:06 -0500

    adding task 1 files for review/help

---
+++ src/main/java/org/custom/task1/dao/CustomerDao.java
@@ -0,0 +1,17 @@
+package org.custom.task1.dao;
+
+import java.util.List;
+
+import org.custom.task1.model.Customer;
+
+public interface CustomerDao {
+	public List<Customer> getAllCustomers() ;
+
+	public Customer getCustomer(int id) ;
+
+	public Customer addCustomer(Customer customer);
+
+	public void updateCustomer(Customer customer) ;
+
+	public void deleteCustomer(int id) ;
+}
---
+++ src/main/java/org/custom/task1/service/CustomerService.java
@@ -0,0 +1,47 @@
+package org.custom.task1.service;
+
+
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.custom.task1.dao.CustomerDao;
+import org.custom.task1.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service("customerService")
+public class CustomerService {
+
+	@Autowired
+	CustomerDao customerDao;
+
+	@Transactional
+	public List<Customer> getAllCustomers() {
+		return customerDao.getAllCustomers();
+	}
+
+	@Transactional
+	public Customer getCustomer(int id) {
+		return customerDao.getCustomer(id);
+	}
+
+	@Transactional
+	public Customer addCustomer(Customer customer) {
+		customerDao.addCustomer(customer);
+		return customer;
+	}
+
+	@Transactional
+	public Customer updateCustomer(Customer customer) {
+		customerDao.updateCustomer(customer);
+		return customer;
+	}
+
+	@Transactional
+	public void deleteCustomer(int id) {
+		customerDao.deleteCustomer(id);
+	}
+}
